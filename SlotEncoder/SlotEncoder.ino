a8986ab43cbb7374437cb7fd649a1784bd0eb148
theapi <petermclarke@gmail.com>
2015-01-11 15:51:50 +0000

    Simpler with attachInterrupt()

--- SlotEncoder/SlotEncoder.ino
+++ SlotEncoder/SlotEncoder.ino
@@ -1,34 +1,19 @@
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
 
 #define PIN_INT_LEFT 2
 #define PIN_DEBUG 13
 
 volatile byte state = LOW;
 
-// Install the interrupt routine.
-ISR(INT0_vect) {
-  // check the value again - since it takes some time to
-  // activate the interrupt routine, we get a clear signal.
-  state = digitalRead(PIN_INT_LEFT);
-  digitalWrite(PIN_DEBUG, state);
-}
 
 void setup() 
 {                
   Serial.begin(57600);
   pinMode(PIN_INT_LEFT, INPUT );
   pinMode(PIN_DEBUG, OUTPUT);
-  //attachInterrupt(0, blink, CHANGE);
+  attachInterrupt(0, blink, CHANGE);
   
   state = digitalRead(PIN_INT_LEFT);
-  
-    Serial.println("Processing initialization");
-  EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
-  EIMSK |= (1 << INT0);     // Turns on INT0
-  Serial.println("Finished initialization");
   
 }
 
@@ -40,6 +25,8 @@ void loop()
 
 void blink()
 {
-  state = !state;
+  // check the value again - since it takes some time to
+  // activate the interrupt routine, we get a clear signal.
+  state = digitalRead(PIN_INT_LEFT);
   digitalWrite(PIN_DEBUG, state);
 }
